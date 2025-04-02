3ce40fa053aa4060e7925c9f711cad906f6c5b9b
Ashley Yael Montiel Vargas <yaelmont@lcg.unam.mx>
2025-04-02 15:09:35 -0600

    E4 Quitara adaptadores
    Script para quitar adaptadores de secuencias

---
+++ src/E1/E4_no_adapters.py
@@ -0,0 +1,18 @@
+#Ejercicio 4 Eliminar adpatadores de secuencias
+
+#Rutas de los archivos 
+input_file = "../data/4_input_adapters.txt"
+output_file = "../results/4_output_adapters.txt"
+
+#Abre el archivo de entrada y salida
+with open(input_file, "r") as infile, open(output_file, "w") as outfile:
+    for line in infile:
+
+        #Cortar adaptadores de la secuencia 4_input_adapters.txt
+        clean_sequence = line.strip()[14:]
+
+        #Manda la salida a un archivo 4_input_no_adapters.txt
+        outfile.write(f"{clean_sequence}\n")
+
+        #Opcion alterna 
+        #print (clean_sequence, file=outfile, end="\n")
